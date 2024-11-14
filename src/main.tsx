d8af128ec4fc0b4e51de8ef1ddf217dde051217a
IDrumsey <claydevelop@gmail.com>
2024-11-14 14:15:33 -0500

    Added theme with different size fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import { createTheme, ThemeProvider } from "@mui/material"
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: "1.476rem",
+    },
+    h2: {
+      fontSize: "1.383rem",
+    },
+    h3: {
+      fontSize: "1.296rem",
+    },
+    h4: {
+      fontSize: "1.215rem",
+    },
+    h5: {
+      fontSize: "1.138rem",
+    },
+    h6: {
+      fontSize: "1.067rem",
+    },
+    body1: {
+      fontSize: "1rem",
+    },
+    body2: {
+      fontSize: "0.937rem",
+    },
+    caption: {
+      fontSize: "0.878rem",
+    },
+  },
+})
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
