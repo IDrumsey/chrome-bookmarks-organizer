76d48e25b9871f931798f3f37ef1f46d9290f0e6
IDrumsey <claydevelop@gmail.com>
2024-11-14 14:52:10 -0500

    Got first stage - click organize button done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import { Box, Button, Typography } from "@mui/material"
+import { Box, Button, CircularProgress, Typography } from "@mui/material"
 import "./App.css"
 import { useState } from "react"
+import Color from "color"
+
+const textColor = new Color("#fff")
 
 function App() {
   const [mode, modeSetter] = useState<"ready" | "results" | "running">("ready")
@@ -13,9 +16,10 @@ function App() {
       paddingTop={4}
       display="flex"
       flexDirection="column"
+      color={textColor.toString()}
     >
       {/* header */}
-      <Box>
+      <Box paddingBottom={4}>
         <Typography
           variant="h2"
           fontWeight="bold"
@@ -24,12 +28,45 @@ function App() {
         </Typography>
       </Box>
 
+      <Box sx={{ flex: 1 }}>
+        {mode == "ready" ? (
+          <Typography
+            variant="caption"
+            sx={{ color: textColor.alpha(0.3).toString() }}
+          >
+            Click the organize button to have AI generate a proposed new
+            organization for your bookmarks
+          </Typography>
+        ) : mode == "running" ? (
+          <Box
+            sx={{
+              position: "absolute",
+              left: "50%",
+              top: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          mode == "results" && (
+            <Typography
+              variant="h6"
+              sx={{ textDecoration: "underline" }}
+            >
+              Proposed organization
+            </Typography>
+          )
+        )}
+      </Box>
+
       {/* footer */}
       <Box sx={{ marginTop: "auto" }}>
         {/* organize button */}
         <Button
           variant="contained"
           fullWidth
+          onClick={() => modeSetter("running")}
         >
           Organize
         </Button>
