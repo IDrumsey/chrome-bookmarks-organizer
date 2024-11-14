e6acdfca7c049bb57fea0a847b65c44fcd7f3bd3
IDrumsey <claydevelop@gmail.com>
2024-11-14 14:37:25 -0500

    Basic layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Typography } from "@mui/material"
 import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [mode, modeSetter] = useState<"ready" | "results" | "running">("ready")
+
   return (
-    <Box>
-      <Typography variant="h2">Bookmark Organizer</Typography>
+    <Box
+      height="100%"
+      boxSizing="border-box"
+      padding={2}
+      paddingTop={4}
+      display="flex"
+      flexDirection="column"
+    >
+      {/* header */}
+      <Box>
+        <Typography
+          variant="h2"
+          fontWeight="bold"
+        >
+          Bookmark Organizer
+        </Typography>
+      </Box>
+
+      {/* footer */}
+      <Box sx={{ marginTop: "auto" }}>
+        {/* organize button */}
+        <Button
+          variant="contained"
+          fullWidth
+        >
+          Organize
+        </Button>
+      </Box>
     </Box>
   )
 }
